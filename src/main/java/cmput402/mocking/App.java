87a130eaf2ba7d5a2967dcfdaf8bc60484b8cbad
shaiful <you@example.com>
2019-01-26 13:49:14 -0700

    initial commit with a maven project and .gitignore

---
+++ src/main/java/cmput402/mocking/App.java
@@ -0,0 +1,13 @@
+package cmput402.mocking;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
